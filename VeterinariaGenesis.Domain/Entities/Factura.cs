85bb3db66b702bf770ba3bcf976083d429cd05c1
YASSER GABRIEL LEZAMA  VADO <yasser.lezama745@std.uni.edu.ni>
2026-05-13 00:39:38 -0600

    Fix: Unified Guid serialization in DetalleFactura to resolve invoice 500 error

--- VeterinariaGenesis.Domain/Entities/Factura.cs
+++ VeterinariaGenesis.Domain/Entities/Factura.cs
@@ -30,6 +30,7 @@ namespace VeterinariaGenesis.Domain.Entities
 
     public class DetalleFactura
     {
+        [BsonRepresentation(MongoDB.Bson.BsonType.String)]
         public Guid Id { get; set; } = Guid.NewGuid();
         [BsonRepresentation(MongoDB.Bson.BsonType.String)]
         public Guid FacturaId { get; set; }
